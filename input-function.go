1ba7c541670a8471cd4b8bc162bc98abe86b7cf2
Billy <billy.davies.10@gmail.com>
2021-03-06 11:07:54 -0500

    Update input-function.go
    formatting updates

--- input-function.go
+++ input-function.go
@@ -10,15 +10,14 @@ import "fmt"
 // output: z.
 func graphFunction(x float64, y float64) float64 {
 	var z float64
-	z = float64(math.Sin(math.Pow(x, 2) + 3*math.Pow(y, 2))/math.Pow(0.1 * math.Sqrt(math.Pow(x, 2) + math.Pow(y,2)), 2)+(math.Pow(x, 2) + 5*math.Pow(y, 2))*math.Exp(1-math.Pow(math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2)), 2))/2)
-    return z
+	z = float64(math.Sin(math.Pow(x, 2)+3*math.Pow(y, 2))/math.Pow(0.1*math.Sqrt(math.Pow(x, 2)+math.Pow(y, 2)), 2) + (math.Pow(x, 2)+5*math.Pow(y, 2))*math.Exp(1-math.Pow(math.Sqrt(math.Pow(x, 2)+math.Pow(y, 2)), 2))/2)
+	return z
 }
-
 
 func main() {
 	var x, y, test float64
 	x = 1
 	y = 0
-	test = graphFunction(x,y)
+	test = graphFunction(x, y)
 	fmt.Println(test)
 }
