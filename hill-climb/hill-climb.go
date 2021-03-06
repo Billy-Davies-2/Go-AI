aeee7145232ec29f480aec8e05f6af8c7b95d6c6
Billy <billy.davies.10@gmail.com>
2021-03-06 11:15:27 -0500

    Create hill-climb.go
    stubbed out hill climbing

---
+++ hill-climb/hill-climb.go
@@ -0,0 +1,10 @@
+package hillclimb
+
+import "math"
+import "container/list"
+
+// Hill climbers algorithm.
+// Gathers all cardinal directions to traverse, then chooses the optimal route.
+func hillClimb(graphFunction func, stepSize float64, xMin float64, xMax float64, yMin float64, yMax float64) list {
+
+}
