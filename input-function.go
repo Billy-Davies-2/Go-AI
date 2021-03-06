cebf3859d9573297c999774429b8d2be0a944354
Billy <billy.davies.10@gmail.com>
2021-03-06 10:57:18 -0500

    Adding graph function

---
+++ input-function.go
@@ -0,0 +1,24 @@
+package graphinput
+
+import "math"
+import "fmt"
+
+// storing the function to vizualize.
+
+// requirments: only can render 3d things for now.
+// input: x (x-axis), y (y-axis)
+// output: z.
+func graphFunction(x float64, y float64) float64 {
+	var z float64
+	z = float64(math.Sin(math.Pow(x, 2) + 3*math.Pow(y, 2))/math.Pow(0.1 * math.Sqrt(math.Pow(x, 2) + math.Pow(y,2)), 2)+(math.Pow(x, 2) + 5*math.Pow(y, 2))*math.Exp(1-math.Pow(math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2)), 2))/2)
+    return z
+}
+
+
+func main() {
+	var x, y, test float64
+	x = 1
+	y = 0
+	test = graphFunction(x,y)
+	fmt.Println(test)
+}
